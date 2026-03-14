9f0cbb386e68acd940227f4eca46b2f49f057d88
Linus <linus.tseng@gmail.com>
2026-03-14 17:12:39 +0800

    Normalize legacy vendor_ids before constraint

--- services/api/migrations/011_softball_supply_vendor_ids_array_constraint.sql
+++ services/api/migrations/011_softball_supply_vendor_ids_array_constraint.sql
@@ -1,5 +1,32 @@
 -- 011_softball_supply_vendor_ids_array_constraint.sql
 
+-- Normalize legacy / malformed vendor_ids values before enforcing array-only constraint.
+update softball_supply_cases
+set vendor_ids = case
+  when vendor_ids is null then
+    case
+      when coalesce(vendor_id, '') <> '' then jsonb_build_array(vendor_id)
+      else '[]'::jsonb
+    end
+  when jsonb_typeof(vendor_ids) = 'array' then vendor_ids
+  when jsonb_typeof(vendor_ids) = 'string' then
+    case
+      when btrim(vendor_ids #>> '{}') <> '' then jsonb_build_array(vendor_ids #>> '{}')
+      when coalesce(vendor_id, '') <> '' then jsonb_build_array(vendor_id)
+      else '[]'::jsonb
+    end
+  when jsonb_typeof(vendor_ids) = 'null' then
+    case
+      when coalesce(vendor_id, '') <> '' then jsonb_build_array(vendor_id)
+      else '[]'::jsonb
+    end
+  else
+    case
+      when coalesce(vendor_id, '') <> '' then jsonb_build_array(vendor_id)
+      else '[]'::jsonb
+    end
+end;
+
 alter table if exists softball_supply_cases
   drop constraint if exists chk_softball_supply_cases_vendor_ids_array;
 
